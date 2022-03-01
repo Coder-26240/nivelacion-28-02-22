f56727120ff3bea0be2a1e615fe8eb31204a7138
JuanMagos <35541642+JuanMagos@users.noreply.github.com>
2022-02-28 19:42:18 -0600

    First commit

---
+++ Clase 1/index.js
@@ -0,0 +1,37 @@
+// const texto = 'Hola ' + 'Mundo';
+const saludo = 'Hola';
+const mundo = 'Mundo';
+const texto = `${saludo} de todo el ${mundo}`;
+
+const nombre = null;
+function hola(event, nombre) {
+  event.preventDefault();
+  alert('coder');
+}
+
+// hola();
+// let number = 0;
+// function suma() {
+//   this.number = number + 1;
+//   number = number + 1; //number++
+//   console.log(number);
+// }
+
+// const resta = () => {
+//   number = number - 1; //number--
+//   console.log(number);
+// };
+// class Auto {
+//   constructor(marca, modelo, color, patente) {
+//     this.marca = marca;
+//     this.modelo = modelo;
+//     this.color = color;
+//     this.patente = patente;
+//   }
+//   mostrarAuto() {
+//     console.log(`${this.marca} ${this.modelo}`);
+//   }
+//   arrancar() {}
+//   fernar() {}
+//   acelerar() {}
+// }
